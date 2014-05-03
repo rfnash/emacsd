56dab77865c7a68cc1897b6fd7086169a0994979
Robert F. Nash <robert@robertnash.net>
2014-05-02 20:05:54 -0400

    Added comments for loading emacs24-starter-kit

--- init.el
+++ init.el
@@ -1,1 +1,6 @@
+;; Emacs24 Starter Kit
+;; (setenv "ORG_HOME" "/home/shared/.emacs.d/el-get/org-mode")
+;; (load-file "~/.emacs.d/emacs24-starter-kit/init.el")
+
+;; Emacs Prelude
 (load-file "~/.emacs.d/prelude/init.el")
