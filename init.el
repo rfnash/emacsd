7f9b4a26336855a6f0f54f2ee704d0a7fe6ccd70
Robert F. Nash <robert@robertnash.net>
2015-01-06 15:24:35 -0500

    added auto-compile
    See https://github.com/tarsius/auto-compile for details.

--- init.el
+++ init.el
@@ -1,3 +1,14 @@
+;;; init.el --- user init file  -*- no-byte-compile: t -*-
+;(add-to-list 'load-path
+;             (expand-file-name "auto-compile"
+;                               (file-name-directory load-file-name)))
+(add-to-list 'load-path "~/.emacs.d/packed")
+(add-to-list 'load-path "~/.emacs.d/auto-compile")
+(setq load-prefer-newer t)
+(require 'auto-compile)
+(auto-compile-on-load-mode 1)
+(auto-compile-on-save-mode 1)
+
 ;; Emacs24 Starter Kit
 ;; (setenv "ORG_HOME" "/home/shared/.emacs.d/el-get/org-mode")
 ;; (load-file "~/.emacs.d/emacs24-starter-kit/init.el")
