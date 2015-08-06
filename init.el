ff16d75f021f71366d3dc6eae0fbb23aa4c1bacd
Robert F. Nash <robert@robertnash.net>
2015-08-05 20:25:33 -0400

    added org keybindings

--- init.el
+++ init.el
@@ -49,8 +49,13 @@
 ;; (require 'diminish)                     ; if you use :diminish
 (require 'bind-key)                     ; if you use any :bind variant
 
+(use-package org
+  :mode ("\\.org\\'" . org-mode)
+  :bind (("C-c l" . org-store-link)
+	 ("C-c c" . org-capture)
+	 ("C-c a" . org-agenda)
+	 ("C-c b" . org-iswitchb)))
 
-(use-package org)
 (require 'org-habit)
 
 (setq org-startup-folded t
