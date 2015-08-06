ac58c44cb666decfe75e298a495046f5e6b265b8
Robert F. Nash <robert@robertnash.net>
2015-08-05 22:40:54 -0400

    moved more config over

--- init.el
+++ init.el
@@ -20,3 +20,15 @@
 (require 'org-install)
 (require 'ob-tangle)
 (org-babel-load-file "~/.emacs.d/rfnash.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(paradox-github-token t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
